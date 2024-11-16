76dbbc1ca825e34b9849060649586d3acb215044
teruma-nanami <kinos.travel.fun@gmail.com>
2024-11-16 10:12:04 +0900

    make up model

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,19 +1,18 @@
-<!-- resources/views/words/index.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Words List</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
+
+@section('content')
+  <div class="container">
     <h1>Words List</h1>
     <a href="{{ route('create') }}">Add New Word</a>
     <a href="{{ route('test') }}">test Word</a>
     <ul>
-        @foreach ($words as $word)
-            <li>{{ $word->english }} - {{ $word->japanese }}</li>
-        @endforeach
+      @foreach ($words as $word)
+        <li>{{ $word->english }} - {{ $word->japanese }}</li>
+      @endforeach
     </ul>
-</body>
-</html>
+  </div>
+@endsection

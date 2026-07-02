9e8b8007009020c21c386d3d343ff1f798ecd5b7
teruma-nanami <kinos.travel.fun@gmail.com>
2026-07-02 14:47:20 +0900

    feat: RepositoryInterfaceのバインドをAppServiceProviderへ追加

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\WordRepositoryInterface;
+use App\Interfaces\WordbookRepositoryInterface;
+use App\Repositories\WordRepository;
+use App\Repositories\WordbookRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(WordRepositoryInterface::class, WordRepository::class);
+        $this->app->bind(WordbookRepositoryInterface::class, WordbookRepository::class);
     }
 
     /**

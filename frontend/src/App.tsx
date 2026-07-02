d7fa462a65bcc7ab4776d930b2067baf979be888
teruma-nanami <kinos.travel.fun@gmail.com>
2026-07-03 05:29:45 +0900

    feat: 単語一覧ページへ共通ナビゲーションヘッダーとRouteを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 import WordList from './components/WordList'
 import './App.css'
 
 function App() {
-  return <WordList />
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<WordList />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App

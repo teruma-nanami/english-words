ef59a2f41f445c10ded2c826d8884a2a977441e4
teruma-nanami <kinos.travel.fun@gmail.com>
2026-07-03 11:05:01 +0900

    feat: App.tsxへ管理用単語一覧ページのRouteを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import AdminWordList from './components/AdminWordList'
 import Header from './components/Header'
 import Test from './components/Test'
 import WordList from './components/WordList'
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WordList />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/admin/words" element={<AdminWordList />} />
       </Routes>
     </>
   )

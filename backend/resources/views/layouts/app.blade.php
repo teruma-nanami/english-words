e33049cc5f834b4ac1aac112a463faa2a78729dc
teruma-nanami <kinos.travel.fun@gmail.com>
2026-07-06 10:55:29 +0900

    feat: グローバルメニューのラベルを認証状態に応じて出し分け

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -25,14 +25,16 @@
           <ul>
             <li><a href="{{ config('services.frontend.url') }}/">単語一覧</a></li>
             <li><a href="{{ config('services.frontend.url') }}/test">単語テスト</a></li>
-            <li><a href="{{ config('services.frontend.url') }}/admin/words">管理</a></li>
             @if (auth()->check())
+              <li><a href="{{ config('services.frontend.url') }}/admin/words" class="header__nav-cta">単語追加</a></li>
               <li>
                 <form action="/logout" method="POST">
                   @csrf
                   <button type="submit" class="logout__button"> <i class="bi bi-box-arrow-right"></i> ログアウト</button>
                 </form>
               </li>
+            @else
+              <li><a href="{{ route('login') }}" class="header__nav-cta">ログイン</a></li>
             @endif
           </ul>
         </div>

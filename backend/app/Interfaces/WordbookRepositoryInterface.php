3e28847e7b4010f20db96ef3ff3f35a71db71756
teruma-nanami <kinos.travel.fun@gmail.com>
2026-07-06 10:36:43 +0900

    feat: WordbookRepositoryInterfaceに更新・削除メソッドを追加

--- backend/app/Interfaces/WordbookRepositoryInterface.php
+++ backend/app/Interfaces/WordbookRepositoryInterface.php
@@ -14,4 +14,8 @@ interface WordbookRepositoryInterface
 	public function getRandomWords(Wordbook $wordbook, int $count): Collection;
 
 	public function create(array $data): Wordbook;
+
+	public function update(Wordbook $wordbook, array $data): Wordbook;
+
+	public function delete(Wordbook $wordbook): void;
 }
